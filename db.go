61e11ef07aff32db899f0f0aa7dc2c088d8aa51d
“dedinirtadinata” <d3di.nirtadinata@gmail.com>
2026-01-11 17:16:57 +0700

    update db

--- db.go
+++ db.go
@@ -9,6 +9,6 @@ import (
 var db *sql.DB
 
 func InitDB() {
-	conn := "host=postgres user=smsuser password=smspassword dbname=smsdb sslmode=disable"
+	conn := "host=postgres-gammu user=smsuser password=smspassword dbname=smsdb sslmode=disable"
 	db, _ = sql.Open("postgres", conn)
 }
