3e7cd22df8f10c9374232b51cb441bbf4648e09d
“dedinirtadinata” <d3di.nirtadinata@gmail.com>
2026-01-11 17:22:24 +0700

    init db

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	// Init database
+	InitDB()
+
 	http.HandleFunc("/send-sms", SendSMSHandler)
+
 	log.Println("SMS API running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
