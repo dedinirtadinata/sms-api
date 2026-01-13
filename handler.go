a1cf01eb928286a6c6709cb67ac071e4b1d101ef
“dedinirtadinata” <d3di.nirtadinata@gmail.com>
2026-01-13 17:31:44 +0700

    update dan sudah fix

--- handler.go
+++ handler.go
@@ -57,7 +57,7 @@ func SendSMSHandler(w http.ResponseWriter, r *http.Request) {
 	// 	req.Message,
 	// )
 
-	_, err := db.Exec(`INSERT INTO "public"."outbox" ("SendBefore", "SendAfter", "Text", "DestinationNumber", "Coding", "UDH", "Class", "TextDecoded",  "MultiPart", "RelativeValidity", "SenderID", "SendingTimeOut", "DeliveryReport", "CreatorID", "Retries", "Priority", "Status", "StatusCode") VALUES ('23:59:59', '00:00:00', NULL,$1, 'Default_No_Compression', NULL, -1, 'Test SMS via SMSD', 'f', -1, NULL, NOW(), 'default', 'SYSTEM', 0, 0, 'Reserved', -1);`, number)
+	_, err := db.Exec(`INSERT INTO "public"."outbox" ("SendBefore", "SendAfter", "Text", "DestinationNumber", "Coding", "UDH", "Class", "TextDecoded",  "MultiPart", "RelativeValidity", "SenderID", "SendingTimeOut", "DeliveryReport", "CreatorID", "Retries", "Priority", "Status", "StatusCode") VALUES ('23:59:59', '00:00:00', NULL,$1, 'Default_No_Compression', NULL, -1, $2, 'f', -1, NULL, NOW(), 'default', 'SYSTEM', 0, 0, 'Reserved', -1);`, number, req.Message)
 
 	if err != nil {
 		http.Error(w, "Failed to queue SMS", 500)
