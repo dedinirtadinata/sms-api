c0421fd40b5b0823754448d4be73d55b9e07aa49
“dedinirtadinata” <d3di.nirtadinata@gmail.com>
2026-01-13 17:17:25 +0700

    update

--- handler.go
+++ handler.go
@@ -44,7 +44,7 @@ func SendSMSHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// number := normalizeNumber(req.To)
+	number := normalizeNumber(req.To)
 
 	// Field "Text" is NULL, only "TextDecoded" is filled (UTF-8 text)
 	// This matches the manual insert pattern that works with Gammu
@@ -57,7 +57,7 @@ func SendSMSHandler(w http.ResponseWriter, r *http.Request) {
 	// 	req.Message,
 	// )
 
-	_, err := db.Exec(`INSERT INTO "public"."outbox" ("UpdatedInDB", "InsertIntoDB", "SendingDateTime", "SendBefore", "SendAfter", "Text", "DestinationNumber", "Coding", "UDH", "Class", "TextDecoded",  "MultiPart", "RelativeValidity", "SenderID", "SendingTimeOut", "DeliveryReport", "CreatorID", "Retries", "Priority", "Status", "StatusCode") VALUES ('2026-01-13 17:07:28', '2026-01-13 17:07:28', '2026-01-13 17:07:28', '23:59:59', '00:00:00', NULL, '+6285228899321', 'Default_No_Compression', NULL, -1, 'Test SMS via SMSD', 'f', -1, NULL, '2026-01-13 17:07:28', 'default', 'SYSTEM', 0, 0, 'Reserved', -1);`)
+	_, err := db.Exec(`INSERT INTO "public"."outbox" ("UpdatedInDB", "InsertIntoDB", "SendingDateTime", "SendBefore", "SendAfter", "Text", "DestinationNumber", "Coding", "UDH", "Class", "TextDecoded",  "MultiPart", "RelativeValidity", "SenderID", "SendingTimeOut", "DeliveryReport", "CreatorID", "Retries", "Priority", "Status", "StatusCode") VALUES ('2026-01-13 17:07:28', '2026-01-13 17:07:28', '2026-01-13 17:07:28', '23:59:59', '00:00:00', NULL,$1, 'Default_No_Compression', NULL, -1, 'Test SMS via SMSD', 'f', -1, NULL, '2026-01-13 17:07:28', 'default', 'SYSTEM', 0, 0, 'Reserved', -1);`, number)
 
 	if err != nil {
 		http.Error(w, "Failed to queue SMS", 500)
